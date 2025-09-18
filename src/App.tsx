152d30e684eecd2dc99f5883b5ee16b7c575ef24
Ayesha Ghani <ayesha.ghani@wavetec.com>
2025-09-18 21:53:28 +0500

    scroll to top issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar";
 import FooterV2 from "./components/FooterV2";
 import DashboardV2 from "./pages/DashboardV2";
-
 import Projects from "./pages/Projects";
 import Lab from "./pages/Lab";
 import Journey from "./pages/Journey";
 import Contact from "./pages/Contact";
 import SideMissions from "./pages/SideMissions";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <ThemeProvider>
+      <ScrollToTop />
       <div className="min-h-screen bg-background text-foreground w-full overflow-x-hidden">
         <Navbar />
         <main className="flex-1 w-full">
